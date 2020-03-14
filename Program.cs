36018607bc9457f03b954547d5c75343a2d2ae9d
fllimon <fallout2408@gmail.com>
2020-03-14 17:48:06 +0200

    Create SimpleLinkedList add two method: AddToBegin, AddToLast

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySimpleLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleLinkedList<string> list = new SingleLinkedList<string>();
+
+            list.AddToBegin("ghkvbh");
+            list.AddToEnd("PreEnd");
+
+            foreach (string item in list)
+            {
+                Console.Write($"\t{item}");
+            }
+            Console.WriteLine();
+
+            list.AddToBegin("ttyfdf");
+            list.AddToBegin("w.z");
+
+            foreach (string item in list)
+            {
+                Console.Write($"\t{item}");
+            }
+            Console.WriteLine();
+
+            list.AddToBegin("Start");
+            list.AddToEnd("End");
+
+            foreach (string item in list)
+            {
+                Console.Write($"\t{item}");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingleLinkedList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySimpleLinkedList
+{
+    class SingleLinkedList<T> : IEnumerable<T>
+    {
+        private Node _first = null;
+        
+        public SingleLinkedList<T> Clone()
+        {
+            return new SingleLinkedList<T> { _first = this._first };
+        }
+
+        public bool IsEmpty()
+        {
+            return (_first == null);
+        }
+
+        public void AddToEnd(T item)
+        {
+            Node node = new Node(item);
+
+            if (IsEmpty())
+            {
+                _first = node;
+            }
+            else
+            {
+                Node current = _first;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                node.Info = item;
+                current.Next = node;
+            }            
+        }
+
+        public void AddToBegin(T item)
+        {
+            Node node = new Node(item);
+
+            node.Info = item;
+            node.Next = _first;
+            _first = node;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node current = _first;
+
+            while (current != null)
+            {
+                yield return current.Info;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        private class Node
+        {
+            #region ======------ PRIVATE DATA -----=====
+
+            private T _item;
+            
+            #endregion
+
+            #region ======----- CTOR -----======
+
+            public Node(T item)
+            {
+                _item = item;
+            }
+
+            #endregion
+
+            #region =====---- PROPERTIES -----=====
+
+            public T Info { get; set; }
+
+            public Node Next { get; set; }
+
+            #endregion
+        }
+    }
+}
